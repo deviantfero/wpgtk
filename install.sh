852c0444a52e92a51b65facd6e4c4b233210ebc6
deviantfero <fmorataya.04@gmail.com>
2015-12-24 15:44:28 -0600

    fixed issue by benwaffle

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mkdir ~/.wallpapers && sudo cp -r ./* /usr/local/bin/ && sudo chmod +x /usr/local/bin/wpg && sudo chmod +x /usr/local/bin/wp
+mkdir ~/.wallpapers & 
+sudo cp -r ./* /usr/local/bin/ && sudo chmod +x /usr/local/bin/wpg && sudo chmod +x /usr/local/bin/wp
