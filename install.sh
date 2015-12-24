33a9436cbfbdc9e0f1b9c9edee6eed2913307267
deviantfero <fmorataya.04@gmail.com>
2015-12-24 01:29:58 -0600

    creates .wallpaper desktop before wp goes for the first time

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo cp -r ./* /usr/local/bin/ && sudo chmod +x /usr/local/bin/wpg && sudo chmod +x /usr/local/bin/wp
+mkdir ~/.wallpapers && sudo cp -r ./* /usr/local/bin/ && sudo chmod +x /usr/local/bin/wpg && sudo chmod +x /usr/local/bin/wp
