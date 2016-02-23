6c70579e9a06dcb62dc3c6571c6881b1a3534d18
deviantfero <fmorataya.04@gmail.com>
2016-02-23 11:06:31 -0600

    fixed a path

--- src/colorparser.py
+++ src/colorparser.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 import fileinput
 from subprocess import call
 from sys import argv
--- src/wpg.py
+++ src/wpg.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 from gi import require_version
 require_version( "Gtk", "3.0" )
 from os import walk
