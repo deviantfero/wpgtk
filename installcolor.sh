7ab1676002c86164a57dac9cfbb0ccd1d1c77348
deviantfero <fmorataya.04@gmail.com>
2016-02-23 01:52:08 -0600

    improved install scripts and added tint2 support

--- installcolor.sh
+++ installcolor.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+function tint2support {
+	echo
+	echo
+	echo "INSTALLING::TINT2-THEME"
+	echo
+	echo "THIS WILL OVERRIDE YOUR TINT2 THEME"
+	echo
+	echo -n "do you want to continue[Y/n]: "
+	read election
+	if [[ $election == "y" || $election == "Y" ]]; then
+		echo -n "INSTALLING::TINT2-THEME"
+		cp ./themes/tint2rc ~/.config/tint2/
+		cp ./themes/tint2rc.base ~/.config/tint2/
+	else
+		echo "TINT2-THEME::NOT-INSTALLED"
+		return 1
+	fi
+}
+
 function install_color {
 	echo "CREATING::DIRECTORIES"
 	mkdir ~/.wallpapers 
@@ -16,6 +36,7 @@ function install_color {
 	cp -r ./themes/flattrcolor ~/.icons
 	echo "INSTALLING::GTK-THEME"
 	sudo cp -r ./themes/FlatColor ~/.themes/
+	tint2support
 	echo "DONE - SET YOUR THEMES AND RUN wpg"
 	sudo chmod +x /usr/local/bin/wpg && sudo chmod +x /usr/local/bin/wp
 }
