8772bc90705d71ec50c183c0d10e1de2ff19262b
deviantfero <fmorataya.04@gmail.com>
2016-02-26 09:44:23 -0600

    uninstall script added

---
+++ uninstall.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function install_dependencies {
+	echo "UNINSTALLING DEPENDENCIES"
+	version="$( uname -r | grep ARCH )"
+
+	if [ -n "$version"  ]; then
+		echo "ARCH LINUX DETECTED::"
+		sh -c "sudo pacman -S python2-pillow feh python-gobject gtk3 libxslt"
+		echo "DEPENDENCIES INSTALL COMPLETE"
+	else
+		version="$( uname -r | grep -Eo "(eneric|64)" )"
+		if [ -n "$version" ]; then
+			echo "DEBIAN OR *BUNTU DETECTED"
+			sh -c "sudo apt-get install feh python3-gi python-gobject python-pip python-imaging xsltproc && pip install Pillow"
+		else
+			echo "ANOTHER DISTRO DETECTED:: INSTALL DEPENDENCIES FOR YOUR DISTRO"
+			echo
+		fi
+	fi
+}
+
+function tint2support {
+	echo
+	echo
+	echo "UNINSTALLING::TINT2-THEME"
+	echo
+	echo "THIS WILL OVERRIDE YOUR TINT2 THEME"
+	echo
+	echo -n "do you want to continue[Y/n]: "
+	read election
+	if [[ "$election" == "y" || "$election" == "Y" ]]; then
+		echo -n "UNINSTALLING::TINT2-THEME"
+		cp ./themes/tint2rc ~/.config/tint2/
+		cp ./themes/tint2rc.base ~/.config/tint2/
+	else
+		echo "TINT2-THEME::NOT-INSTALLED"
+		return 1
+	fi
+}
+
+function uninstall_color {
+	echo "UNINSTALLING::WPG"
+	sudo rm -r /usr/local/bin/py/
+	sudo rm -r /usr/local/bin/src/
+	sudo rm /usr/local/bin/wpg
+	sudo rm /usr/local/bin/functions
+	echo "UNUNINSTALLING::OPENBOX-THEME"
+	rm -r ~/themes/colorbamboo 
+	rm -r ~/themes/colorbamboo_nb
+	echo "UNINSTALLING::ICONS"
+	rm -r ~/themes/flattrcolor
+	echo "UNINSTALLING::GTK-THEME"
+	rm -r ~/themes/FlatColor
+	tint2support
+	echo "DONE - SET YOUR THEMES AND RUN wpg"
+}
+
+uninstall_color
