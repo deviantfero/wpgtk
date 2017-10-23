bc0804212922eead064325a217b65b66a2ca1f26
Matthias Riße <walking.toast99@gmail.com>
2017-10-23 13:47:06 +0200

    added autocomplete functionality for bash and zsh

--- setup.py
+++ setup.py
@@ -48,5 +48,8 @@ setuptools.setup(
     ],
     include_package_data=True,
     data_files=[('etc/wpgtk', ['wpgtk/misc/wpg.conf']),
-                ('bin/', ['wpgtk/misc/wpg-install.sh'])]
+                ('bin/', ['wpgtk/misc/wpg-install.sh']),
+                ('usr/share/bash-completion/completions', ['completions/bash/wpg'])]
+    if(shutil.which("zsh") != None):
+        data_files.append(('usr/bin/share/zsh/site-functions/', ['completions/zsh/_wpg'])
 )
