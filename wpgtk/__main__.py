02c06794c0fb3d172851a5b9c2734680eb00b544
deviantfero <fmorataya.04@gmail.com>
2018-03-19 01:33:38 -0600

    add new flag to apply json colorschemes to wallpapers

--- wpgtk/__main__.py
+++ wpgtk/__main__.py
@@ -4,7 +4,7 @@ import random
 import wpgtk.data.config as config
 from os import path
 from subprocess import Popen
-from wpgtk.data import files, themer
+from wpgtk.data import files, themer, logger, color
 from wpgtk.data.config import __version__
 try:
     from wpgtk.gui import theme_picker
@@ -16,52 +16,68 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-s',
-                        help='set the wallpaper and colorscheme, apply \
-                        changes system-wide',
-                        nargs='*')
-    parser.add_argument('-r',
-                        help='restore the wallpaper and colorscheme',
-                        action='store_true')
-    parser.add_argument('-m',
-                        help='pick a random wallpaper and set it',
-                        action='store_true')
-    parser.add_argument('-a',
-                        help='add images to the wallpaper folder and generate \
-                        colorschemes',
-                        nargs='*')
+    parser.add_argument("-s",
+                        help="set the wallpaper and colorscheme, apply changes system-wide",
+                        nargs="*")
+
+    parser.add_argument("-r",
+                        help="restore the wallpaper and colorscheme",
+                        action="store_true")
+
+    parser.add_argument("-m",
+                        help="pick a random colorscheme and set it, specify wallpaper to avoid changing it",
+                        const="random_both", nargs="?")
+
+    parser.add_argument("-a",
+                        help="add a wallpaper and generate a colorscheme",
+                        nargs="*")
+
     parser.add_argument('-l',
-                        help='see which wallpapers are available',
-                        action='store_true')
-    parser.add_argument('--version',
-                        help='print the current version',
-                        action='store_true')
-    parser.add_argument('-d',
-                        help='delete the wallpaper(s) from wallpaper folder',
-                        nargs='*')
-    parser.add_argument('-c',
-                        help='shows the current wallpaper',
-                        action='store_true')
-    parser.add_argument('-z',
-                        help='shuffles the given colorscheme(s)',
-                        nargs='*')
-    parser.add_argument('-t',
-                        help='send color sequences to all terminals VTE true',
-                        action='store_true')
-    parser.add_argument('-x',
-                        help='add, remove and list \
-                             templates instead of themes',
-                        action='store_true')
-    parser.add_argument('-y',
-                        help='add an existent basefile template [config, basefile]',
+                        help="see which wallpapers are available",
+                        action="store_true")
+
+    parser.add_argument("--version",
+                        help="print the current version",
+                        action="store_true")
+
+    parser.add_argument("-d",
+                        help="delete the wallpaper(s) from wallpaper folder",
+                        nargs="*")
+
+    parser.add_argument("-c",
+                        help="shows the current wallpaper",
+                        action="store_true")
+
+    parser.add_argument("-z",
+                        help="shuffles the given colorscheme(s)",
+                        nargs="*")
+
+    parser.add_argument("-T",
+                        help="asign a theme in json format to wallpaper [wallpaper, json]",
+                        nargs="*")
+
+    parser.add_argument("-t",
+                        help="send color sequences to all terminals VTE true",
+                        action="store_true")
+
+    parser.add_argument("-x",
+                        help="add, remove and list templates instead of themes",
+                        action="store_true")
+
+    parser.add_argument("-y",
+                        help="add an existent basefile template [config, basefile]",
                         nargs='*')
 
-    config.init()
     args = parser.parse_args()
+
+    if args.m == "random_both":
+        filename = random.choice(files.get_file_list())
+        themer.set_theme(filename, filename)
+        exit(0)
 
     if args.m:
         filename = random.choice(files.get_file_list())
-        themer.set_theme(filename, filename)
+        themer.set_theme(args.m, filename)
         exit(0)
 
     if args.s:
@@ -69,30 +85,30 @@ def main():
             try:
                 themer.set_theme(args.s[0], args.s[0], args.r)
             except TypeError as e:
-                print('ERR:: file ' + args.s[0] + ' not found')
+                logger.log.error("file " + args.s[0] + " not found")
                 raise e
         elif len(args.s) == 2:
             try:
                 themer.set_theme(args.s[0], args.s[1], args.r)
             except TypeError:
-                print('ERR:: file  not found')
+                logger.log.error("file  not found")
         elif len(args.s) > 2:
-            print('ERR:: Specify just 2 filenames')
+            logger.log.error("specify just 2 filenames")
 
     if args.l:
         if args.x:
             templates = files.get_file_list(config.OPT_DIR, False)
-            [print(t) for t in templates if '.base' in t]
+            [print(t) for t in templates if ".base" in t]
         else:
             files.show_files()
         exit(0)
 
     if args.t:
-        Popen(['cat', path.join(config.WPG_DIR, 'sequences')])
+        Popen(["cat", path.join(config.WPG_DIR, "sequences")])
         exit(0)
 
     if args.version:
-        print('current version: ' + __version__)
+        print("current version: " + __version__)
         exit(0)
 
     if args.d:
@@ -118,7 +134,7 @@ def main():
         for arg in args.z:
             themer.shuffle_colors(arg)
             themer.auto_adjust_theme(arg)
-            print('OK:: shuffled %s' % arg)
+            logger.log.info("shuffled %s" % arg)
         exit(0)
 
     if args.y:
@@ -129,8 +145,16 @@ def main():
         try:
             theme_picker.run(args)
         except NameError:
-            print('ERR:: missing pygobject module, use cli', file=sys.stderr)
+            logger.log.error("missing pygobject module, use cli")
+
+    if args.T:
+        if len(args.T) != 2:
+            logger.log.error("specify a wallpaper and a colorscheme json")
+            exit(1)
+        color.write_colors(args.T[0], color.get_color_list(args.T[1], True))
+        logger.log.info("applied %s to %s" % (args.T[1], args.T[0]))
 
 
 if __name__ == "__main__":
+    config.init()
     main()
