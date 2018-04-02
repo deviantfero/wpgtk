52c472d7f2c00f1f763777f976915c16f87125e3
deviantfero <fmorataya.04@gmail.com>
2018-04-02 00:15:21 -0600

    better sample images

--- wpgtk/data/sample.py
+++ wpgtk/data/sample.py
@@ -10,13 +10,18 @@ import pywal
 
 
 def create_sample(colors, f=os.path.join(config.WALL_DIR, ".tmp.sample.png")):
-    im = Image.new("RGB", (1000, 100), "white")
+    im = Image.new("RGB", (500, 50), "white")
     pix = im.load()
 
-    width_sample = im.size[0]//len(colors)
+    width_sample = im.size[0]//(len(colors)//2)
 
-    for i, c in enumerate(colors):
+    for i, c in enumerate(colors[:8]):
         for j in range(width_sample*i, width_sample*i+width_sample):
-            for k in range(0, 100):
+            for k in range(0, 25):
+                pix[j, k] = pywal.util.hex_to_rgb(c)
+
+    for i, c in enumerate(colors[8:]):
+        for j in range(width_sample*i, width_sample*i+width_sample):
+            for k in range(25, 50):
                 pix[j, k] = pywal.util.hex_to_rgb(c)
     im.save(f)
