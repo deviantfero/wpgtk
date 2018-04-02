f81b5dc73eac77b2c5ff7ef2ee1aa6b72c87449a
deviantfero <fmorataya.04@gmail.com>
2018-04-02 03:31:43 -0600

    delete all colorschemes related to one wallpaper, get sample path and cache path

--- wpgtk/data/files.py
+++ wpgtk/data/files.py
@@ -38,12 +38,19 @@ def show_files(path=config.WALL_DIR, images=True):
     print("\n".join(get_file_list(path, images)))
 
 
-def get_cache_filename(wallpaper):
+def get_cache_path(wallpaper, backend=None):
+    if not backend:
+        backend = config.wpgtk.get('backend', 'wal')
     filepath = join(config.WALL_DIR, wallpaper)
-    cache_filename = cache_fname(filepath,
-                                 config.wpgtk.get('backend', 'wal'),
-                                 False, config.WALL_DIR)
+    cache_filename = cache_fname(filepath, backend, False, config.WALL_DIR)
     return join(*cache_filename)
+
+
+def get_sample_path(wallpaper, backend=None):
+    if not backend:
+        backend = config.wpgtk.get('backend', 'wal')
+    sample_filename = "%s_%s_sample.png" % (wallpaper, backend)
+    return join(config.SAMPLE_DIR, sample_filename)
 
 
 def add_template(cfile, basefile=None):
@@ -51,33 +58,32 @@ def add_template(cfile, basefile=None):
     cfile = os.path.realpath(cfile)
     # we remove dots from possible dotfiles
     if not basefile:
-        l = [atom.lstrip('.') for atom in cfile.split('/')
+        l = [atom.lstrip(".") for atom in cfile.split("/")
              if atom is not 'home']
         if len(l) > 3:
             l = l[-3::]
-        templatename = '.'.join(l) + '.base'
+        templatename = ".".join(l) + ".base"
     else:
         templatename = basefile.split('/').pop()
     logging.info('added ' + templatename + '@' + cfile)
     try:
-        logging.info('MAKING BACKUP CONFIG')
-        shutil.copy2(cfile, cfile + '.bak')
-        logging.info('CREATING BASE')
+        logging.info("creating backup %s.bak" % cfile)
+        shutil.copy2(cfile, cfile + ".bak")
+        logging.info("creating base file")
         if basefile:
             shutil.copy2(basefile, join(config.OPT_DIR, templatename))
         else:
             shutil.copy2(cfile, join(config.OPT_DIR, templatename))
-        logging.info('CREATING SYMLINK')
+        logging.info("linking template to original file")
         os.symlink(cfile, join(config.OPT_DIR,
-                   templatename.replace('.base', '')))
+                   templatename.replace(".base", "")))
     except Exception as e:
         logging.error(str(e.strerror))
 
 
 def delete_template(basefile):
     basefile_path = join(config.OPT_DIR, basefile)
-    configfile_path = basefile_path.replace('.base', '')
-
+    configfile_path = basefile_path.replace(".base", "")
     try:
         os.remove(basefile_path)
         if os.path.islink(configfile_path):
@@ -87,12 +93,10 @@ def delete_template(basefile):
 
 
 def delete_colorschemes(wallpaper):
-    filepath = join(config.WALL_DIR, wallpaper)
     for backend in list_backends():
-        cache_args = [filepath, backend, False, config.WALL_DIR]
-        json_file = cache_fname(*cache_args)
         try:
-            os.remove(join(*json_file))
+            os.remove(get_cache_path(wallpaper, backend))
+            os.remove(get_sample_path(wallpaper, backend))
         except OSError:
             pass
 
