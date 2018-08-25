4bbbff3423c7ce1191abb26c6ed9951c5ac375ef
deviantfero <fmorataya.04@gmail.com>
2018-08-24 19:11:57 -0600

    replace single quotes

--- wpgtk/data/util.py
+++ wpgtk/data/util.py
@@ -8,7 +8,7 @@ from pywal.util import rgb_to_hex, hex_to_rgb
 
 
 def get_distance(hex_src, hex_tgt):
-    "gets color distance between to hex values"
+    """gets color distance between to hex values"""
     r1, g1, b1 = hex_to_rgb(hex_src)
     r2, g2, b2 = hex_to_rgb(hex_tgt)
 
@@ -16,7 +16,7 @@ def get_distance(hex_src, hex_tgt):
 
 
 def get_hls_val(hexv, what):
-    "gets a color in hue light and saturation format"
+    """gets a color in hue light and saturation format"""
     whatdict = {'hue': 0, 'light': 1, 'sat': 2}
     hls = hex_to_hls(hexv)
 
@@ -24,8 +24,8 @@ def get_hls_val(hexv, what):
 
 
 def set_hls_val(hexv, what, val):
-    "assign a value to a hls color and return a
-    converted hex value"
+    """assign a value to a hls color and return a
+    converted hex value"""
     whatdict = {'hue': 0, 'light': 1, 'sat': 2}
     hls = list(hex_to_hls(hexv))
 
@@ -34,13 +34,13 @@ def set_hls_val(hexv, what, val):
 
 
 def hex_to_hls(hex_string):
-    "convert a hex value to hls coordinates"
+    """convert a hex value to hls coordinates"""
     r, g, b = hex_to_rgb(hex_string)
     return rgb_to_hls(r, g, b)
 
 
 def hls_to_hex(hls):
-    "convert a hls coordinate to hex code"
+    """convert a hls coordinate to hex code"""
     h, l, s = hls
     r, g, b = hls_to_rgb(h, l, s)
     rgb_int = [max(min(int(elem), 255), 0) for elem in [r, g, b]]
@@ -49,7 +49,7 @@ def hls_to_hex(hls):
 
 
 def alter_brightness(hex_string, amount, sat=0):
-    "alters amount of light and saturation in a color"
+    """alters amount of light and saturation in a color"""
     h, l, s = hex_to_hls(hex_string)
     l = max(min(l + amount, 255), 1)
     s = min(max(s - sat, -1), 0)
