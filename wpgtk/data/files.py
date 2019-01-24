be3e392cf72818eaa7e9abf5d67e52f2c4a7b671
deviantfero <fmorataya.04@gmail.com>
2019-01-24 13:29:01 -0600

    write_script function for writing the script to execute at startup

--- wpgtk/data/files.py
+++ wpgtk/data/files.py
@@ -26,10 +26,24 @@ def get_file_list(path=WALL_DIR, images=True):
         return files
 
 
+def write_script(wallpaper, colorscheme):
+    """writes the script that should be called on startup
+    to restore the theme."""
+    set_wall = settings.getboolean("set_wallpaper", True)
+    light_theme = settings.getboolean("light_theme", True)
+
+    flags = "-rs" if set_wall else "-nrs"
+    flags += "L" if light_theme else ""
+
+    with open(join(WPG_DIR, "wp_init.sh"), "w") as script:
+        command = "wpg %s '%s' '%s'" % (flags, wallpaper, colorscheme)
+        script.writelines(["#!/usr/bin/env bash\n", command])
+
+
 def get_cache_path(wallpaper, backend=None):
     """get a colorscheme cache path using a wallpaper name"""
     if not backend:
-        backend = settings.get('backend', 'wal')
+        backend = settings.get("backend", "wal")
 
     filepath = join(WALL_DIR, wallpaper)
     filename = cache_fname(filepath, backend, False, WPG_DIR)
