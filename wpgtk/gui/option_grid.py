2c64ed79f990f1506d1e20509ad9e54331f1977c
lelobster <lelobster@protonmail.com>
2020-02-22 00:05:03 +0100

    added option to disable calling themer.py's reload.all()

--- wpgtk/gui/option_grid.py
+++ wpgtk/gui/option_grid.py
@@ -110,6 +110,14 @@ class OptionsGrid(Gtk.Grid):
         )
         self.lbl_auto_adjust = Gtk.Label("Always auto adjust")
 
+        self.reload_switch = Gtk.Switch()
+        self.reload_switch.connect(
+            "notify::active",
+            self.on_activate,
+            "do_reload"
+        )
+        self.lbl_reload = Gtk.Label("Reload system")
+
         # edit cmd
         self.editor_lbl = Gtk.Label("Open optional files with:")
         self.editor_txt = Gtk.Entry()
@@ -155,6 +163,9 @@ class OptionsGrid(Gtk.Grid):
 
         self.switch_grid.attach(self.lbl_vte, 5, 3, 3, 1)
         self.switch_grid.attach(self.vte_switch, 9, 3, 1, 1)
+
+        self.switch_grid.attach(self.lbl_reload, 5, 4, 3, 1)
+        self.switch_grid.attach(self.reload_switch, 9, 4, 1, 1)
 
         # Active Grid attach
         self.active_grid.attach(self.backend_lbl, 1, 1, 1, 1)
@@ -202,6 +213,8 @@ class OptionsGrid(Gtk.Grid):
             .set_active(settings.getboolean("smart_sort", True))
         self.auto_adjust_switch\
             .set_active(settings.getboolean("auto_adjust", False))
+        self.reload_switch\
+            .set_active(settings.getboolean("do_reload", True))
 
         self.editor_txt\
             .set_text(settings.get("editor", "urxvt -e vim"))
