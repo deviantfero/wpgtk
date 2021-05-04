b96d4a85291def4be479af87a3101fb514539808
deviantfero <fmorataya.04@gmail.com>
2021-05-03 20:51:58 -0600

    Streamline keyword saving between colorscheme keyword file or default

--- wpgtk/data/keywords.py
+++ wpgtk/data/keywords.py
@@ -1,7 +1,7 @@
 import configparser
 from os import path
 
-from .config import user_keywords
+from .config import user_keywords, write_conf
 from .files import get_keywords_path
 
 KEY_LENGTH = 5
@@ -19,76 +19,81 @@ def create_keywords_file(colorscheme):
     with open(get_keywords_path(colorscheme), "w") as keyword_file:
         parser.write(keyword_file)
 
-    return parser
+    return get_keywords_path(colorscheme)
 
 
-def get_keywords_parser(colorscheme):
+def get_keywords_section(colorscheme):
     """get keyword file configparser for current wallpaper
        or create one if it does not exist"""
 
-    if colorscheme == None:
-        parser = user_keywords
-        return parser
+    if colorscheme is None:
+        return user_keywords
 
-    if path.isfile(get_keywords_path(colorscheme)):
-        parser = configparser.ConfigParser()
-        parser.read(get_keywords_path(colorscheme))
+    parser = configparser.ConfigParser()
 
-        return parser
+    if not path.isfile(get_keywords_path(colorscheme)):
+        create_keywords_file(colorscheme)
 
-    else:
-        return create_keywords_file(colorscheme)
+    parser.read(get_keywords_path(colorscheme))
+    return parser['keywords']
 
 
-def update_key(colorscheme, old_keyword, new_keyword):
+def update_key(old_keyword, new_keyword, colorscheme=None):
     """validates and updates a keyword for a wallpaper"""
     if not new_keyword:
         raise Exception('Keyword must be longer than 5 characters')
 
-    parser = get_keywords_parser(colorscheme) if colorscheme else user_keywords
-    keywords = parser['keywords']
+    keywords = get_keywords_section(colorscheme)
     keywords[new_keyword] = keywords[old_keyword]
 
     if (old_keyword != new_keyword):
         keywords.pop(old_keyword, None)
 
-    with open(get_keywords_path(colorscheme), "w") as keyword_file:
-        parser.write(keyword_file)
+    write_keyword_file(keywords, colorscheme)
 
 
-def update_value(colorscheme, keyword, value):
+def update_value(keyword, value, colorscheme=None):
     """update the value to replace the user defined keyword with"""
     if not value:
         raise Exception('Value must exist')
 
-    parser = get_keywords_parser(colorscheme) if colorscheme else user_keywords
-    keywords = parser['keywords']
+    keywords = get_keywords_section(colorscheme)
     keywords[keyword] = value
 
-    with open(get_keywords_path(colorscheme), "w") as keyword_file:
-        parser.write(keyword_file)
+    write_keyword_file(keywords, colorscheme)
 
 
-def create_pair(colorscheme, keyword, value):
+def create_pair(keyword, value, colorscheme=None):
     """create a key value pair for a wallpaper"""
+    print('hi', keyword, value, colorscheme)
     if not value:
         raise Exception('There must be a value')
 
     if not keyword:
         raise Exception('There must be a keyword')
 
-    parser = get_keywords_parser(colorscheme) if colorscheme else user_keywords
-    parser['keywords'][keyword] = value
+    keywords = get_keywords_section(colorscheme)
+    keywords[keyword] = value
 
-    with open(get_keywords_path(colorscheme), "w") as keyword_file:
-        parser.write(keyword_file)
+    write_keyword_file(keywords, colorscheme)
 
 
-def remove_pair(colorscheme, keyword):
+def remove_pair(keyword, colorscheme=None):
     """removes a pair of keyword value for a wallpaper"""
 
-    parser = get_keywords_parser(colorscheme) if colorscheme else user_keywords
-    parser['keywords'].pop(keyword, None)
+    keywords = get_keywords_section(colorscheme)
+    keywords.pop(keyword, None)
 
-    with open(get_keywords_path(colorscheme), "w") as keyword_file:
-        parser.write(keyword_file)
+    write_keyword_file(keywords, colorscheme)
+
+
+def write_keyword_file(keywords, colorscheme=None):
+    if colorscheme:
+        keywords_path = get_keywords_path(colorscheme)
+        parser = configparser.ConfigParser()
+        parser['keywords'] = keywords
+
+        with open(keywords_path, "w") as keyword_file:
+            parser.write(keyword_file)
+    else:
+        write_conf()
