634d698976f2d026810de1f0be54e0b5a069fbd7
Alexis Maiquez <almamu@almamu.com>
2022-10-28 01:56:16 +0200

    Fix order of calls to require_version and added missing calls
    Signed-off-by: Alexis Maiquez <almamu@almamu.com>

--- wpgtk/gui/color_grid.py
+++ wpgtk/gui/color_grid.py
@@ -11,8 +11,10 @@ from ..data import themer
 
 from .color_picker import ColorDialog
 from gi import require_version
-from gi.repository import Gtk, Gdk, GdkPixbuf
 require_version("Gtk", "3.0")
+require_version("Gdk", "3.0")
+require_version('GdkPixbuf', '3.0')
+from gi.repository import Gtk, Gdk, GdkPixbuf
 
 # TODO: remove current_walls call, use simple ist
 # TODO: use simple text combo
